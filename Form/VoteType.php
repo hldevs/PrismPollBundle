fbfa68bdb6957cae1f06e29e7b4303909fd73875
Ras <ras@dpro.pl>
2016-04-20 08:42:40 +0000

    refs #4127 ankieta, update formularza dla sf3

--- Form/VoteType.php
+++ Form/VoteType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotNull;
-use Symfony\Component\Validator\Constraints\Choice;
 
 /**
  * VoteType
